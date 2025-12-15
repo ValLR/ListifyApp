fd2a2818e4d7027019f0c0a02dee544726470dbd
ValLR <valentinalopezrecabal@gmail.com>
2025-12-15 19:32:25 -0300

    fix: take out pauses

--- cypress/e2e/complete_flow.cy.ts
+++ cypress/e2e/complete_flow.cy.ts
@@ -1,5 +1,3 @@
-const pause = (ms = 1200) => cy.wait(ms);
-
 describe('E2E: Complete User Flow (Demo)', () => {
 
   beforeEach(() => {
@@ -7,7 +5,6 @@ describe('E2E: Complete User Flow (Demo)', () => {
     cy.clearAllSessionStorage?.();
     cy.viewport(393, 851);
     cy.visit('http://localhost:8100');
-    pause(1500);
   });
 
   it('Registers, logs in, manages lists and logs out', () => {
@@ -15,22 +12,17 @@ describe('E2E: Complete User Flow (Demo)', () => {
     cy.contains('Registrarse', { timeout: 10000 })
       .should('be.visible')
       .click();
-    pause(800);
 
     cy.url().should('include', '/register');
-    pause(500);
 
     cy.get('ion-input[formControlName="usuario"] input')
       .type('ValentinaTest', { force: true });
-    pause(800);
 
     cy.get('ion-input[formControlName="password"] input')
       .type('1234', { force: true });
-    pause(800);
 
     cy.get('ion-input[formControlName="confirmPassword"] input')
       .type('1234', { force: true });
-    pause(800);
 
     cy.get('ion-select[formControlName="region"]').then(($sel) => {
       const el = $sel[0] as any;
@@ -45,14 +37,11 @@ describe('E2E: Complete User Flow (Demo)', () => {
     cy.get('ion-select[formControlName="region"]').should(($el) => {
       expect(($el[0] as any).value).to.eq('valparaiso');
     });
-    pause(1200);
 
     cy.get('#open-date-modal').click({ force: true });
-    pause(1200);
 
     cy.get('ion-modal.datetime-modal', { timeout: 10000 })
       .should('be.visible');
-    pause(800);
 
     cy.get('ion-modal.datetime-modal ion-datetime').then(($dt) => {
       const el = $dt[0] as any;
@@ -63,17 +52,14 @@ describe('E2E: Complete User Flow (Demo)', () => {
         composed: true,
       }));
     });
-    pause(800);
 
     cy.get('ion-modal.datetime-modal ion-datetime')
       .shadow()
       .contains('Confirmar')
       .click({ force: true });
-    pause(1200);
 
     cy.get('ion-modal.datetime-modal')
       .should('have.class', 'overlay-hidden');
-    pause(1000);
 
     cy.contains('ion-button', 'REGISTRARME', { timeout: 10000 })
       .should('be.visible')
@@ -84,79 +70,60 @@ describe('E2E: Complete User Flow (Demo)', () => {
         cy.wrap($btn).click({ force: true });
       });
 
-    pause(2000);
     cy.url({ timeout: 15000 }).should('include', '/login');
-    pause(200);
 
     // Login
     cy.get('ion-input[name="username"] input')
       .type('ValentinaTest', { force: true });
-    pause(800);
 
     cy.get('ion-input[name="password"] input')
       .type('1234', { force: true });
-    pause(800);
 
     cy.contains('Ingresar')
       .click({ force: true });
-    pause(2000);
 
     cy.url({ timeout: 15000 }).should('include', '/home');
     cy.contains('Mis Listas').should('be.visible');
-    pause(500);
 
     // Create list
     cy.contains('Crear una lista')
       .click({ force: true });
-    pause(1200);
 
     cy.get('ion-alert').should('be.visible');
-    pause(800);
 
     cy.get('ion-alert input.alert-input')
       .type('Lista Prueba', { force: true });
-    pause(800);
 
     cy.get('ion-alert button')
       .contains('Crear')
       .click({ force: true });
-    pause(1500);
 
     cy.contains('Lista Prueba')
       .should('be.visible')
       .click({ force: true });
-    pause(800);
 
     // Add item
     cy.get('ion-input[placeholder="Ej: Leche, Pan..."] input')
       .type('Pizza', { force: true });
-    pause(800);
 
     cy.get('ion-icon[name="add-circle"]')
       .click({ force: true });
-    pause(1200);
 
     cy.contains('Pizza').should('be.visible');
-    pause(500);
 
     cy.get('ion-back-button')
       .click({ force: true });
-    pause(500);
 
     // Profile and logout
     cy.get('ion-icon[name="person-circle-outline"]')
       .click({ force: true });
-    pause(1000);
 
     cy.contains('Mi Perfil').should('be.visible');
-    pause(1000);
 
     cy.contains('Cerrar sesión')
       .click({ force: true });
-    pause(1500);
 
     cy.url({ timeout: 15000 }).should('include', '/login');
-    pause(200);
   });
 
 it('Must manage unexistant routes (404)', () => {
